4414e0119776bc73eb50d4e238d91f5ef475bfbe
Emmanuel B <blancocemmanuel@gmail.com>
2026-01-30 18:58:43 +1100

    css commit

--- src/searchresults/SearchResults.jsx
+++ src/searchresults/SearchResults.jsx
@@ -1,4 +1,5 @@
 import resultst from '../cssModules/resultstyles.module.css';
+import { Plus } from "lucide-react";
 
 export default function SearchResults(props) {
 
@@ -8,9 +9,15 @@ return (
     <div className={resultst.searchresults}>
      <ul>
       {props.result.map(({ id, artist, name, album }) => (
-  <li key={id}>
-   Name: {name}, Artist: {artist}, Album: {album}
-    <button onClick={() => props.handleCheck(id, true)}>+</button>
+  <li key={id} className={resultst.list}>
+   <span className={resultst.text}>
+    <span className={resultst.label}>Song:&nbsp;</span> 
+    {name}, <span className={resultst.label}>Artist:&nbsp;</span> {artist}, <span className={resultst.label}>Album:&nbsp;</span> {album}
+  </span>
+
+  <button onClick={() => props.handleCheck(id, true)}>
+    <Plus size={16} />
+  </button>
 </li>
      ))
      }
@@ -20,3 +27,8 @@ return (
 )
 };
 
+/*
+&nbsp; = non-breaking space
+A space where the browser is not allowed to break the line.
+the browser sees this as one unbreakable gap between Song: and the first word of {name}
+ */
